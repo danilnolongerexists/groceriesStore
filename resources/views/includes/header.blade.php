3c01b64927681fa047a47fa372c8f1994de8912d
danilnolongerexists <danilgrinin@gmail.com>
2025-02-12 11:14:38 +0500

    fixed some things

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -237,14 +237,16 @@
 @endif
 
 @if (session('success'))
-    <div class="alert alert-success">
+    <div id="flash-message" class="alert alert-success">
         {{ session('success') }}
     </div>
 @endif
 
 @if (session('error'))
-    <div class="alert alert-danger">
+    <div id="flash-message" class="alert alert-danger">
         {{ session('error') }}
     </div>
 @endif
+
+
 </header>
